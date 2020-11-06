c498701622aa6e632c276a107601781ef6f34833
wayne1512 <57416683+wayne1512@users.noreply.github.com>
2020-11-07 00:17:47 +0100

    fixed video with webgl

---
+++ asteroids/Assets/scripts/BackgroundVidStarter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class BackgroundVidStarter : MonoBehaviour
+{
+    private void Awake()
+    {
+        VideoPlayer videoPlayer = GetComponent<VideoPlayer>();
+
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            videoPlayer.url = "StreamableAssets/star.mp4";
+        }
+    }
+
+    private void Update()
+    {
+        Debug.Log("video player playing: " + GetComponent<VideoPlayer>().isPlaying);
+    }
+}
